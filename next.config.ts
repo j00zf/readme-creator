e91ce5688a3a5645a25f106ab6d3c59786cb34bd
Joseph Sebastian <josephsebastian282003@gmail.com>
2026-01-25 23:02:00 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: false, // 🚫 disable Turbopack (CRITICAL for Termux)
-  },
-
   devIndicators: false,
 
   webpack(config) {
-    // reduce memory usage on Android
     config.cache = false;
     return config;
   },
